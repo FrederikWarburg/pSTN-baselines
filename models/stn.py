9ab643acac146391fa2b9ea30e7201819219d647
Frederik Warburg <frederikwarburg@Frederiks-MacBook-Pro-10.local>
2019-12-09 18:28:18 +0100

    bug fix

--- models/stn.py
+++ models/stn.py
@@ -72,7 +72,7 @@ class STN(nn.Module):
         # [im1, im2, im3] => [im1, im1, im2, im2, im3, im3]
         # [theta1, theta2, theta3] => [theta1[:num_params],theta1[num_params:], theta2[:num_params],theta2[num_params:],theta3[:num_params],theta3[num_params:]
         theta_split = torch.zeros((batch_size*self.N,self.num_param), device=theta.device) #[b * N, num_params]
-        x_split = torch.zeros((batch_size*self.N, channels,height,width)) #[b*N, im]
+        x_split = torch.zeros((batch_size*self.N, channels,height,width), device=theta.device) #[b*N, im]
 
         for b in range(batch_size):
             for i in range(self.N):
