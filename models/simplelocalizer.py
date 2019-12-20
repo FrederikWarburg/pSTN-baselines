04ebea3af0efa8c651a8fc037f996411981177d5
Frederik Warburg <frederikwarburg@Frederiks-MacBook-Pro-10.local>
2019-12-20 16:38:33 +0100

    test mnist experiment on cluster

--- models/simplelocalizer.py
+++ models/simplelocalizer.py
@@ -38,7 +38,7 @@ class SimpleLocalizer(nn.Module):
 
             # Tiling
             bias = torch.tensor([[-1,-1],[1,1],[1,-1],[-1,1]], dtype=torch.float)*0.5
-            self.fc2.bias.data.copy_(bias[:self.N].view(-1))
+            self.fc_loc[2].bias.data.copy_(bias[:self.N].view(-1))
         if self.num_param == 6:
             self.fc_loc[2].bias.data.copy_(torch.tensor([1,0,0,
                                                          0,1,0]*self.N, dtype=torch.float))
