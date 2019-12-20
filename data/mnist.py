69d0ff42f3dda1e564d5ca4d9961653014f20a99
Frederik Warburg <frederikwarburg@Frederiks-MacBook-Pro-10.local>
2019-12-20 14:53:31 +0100

    test mnist experiment on cluster

--- data/mnist.py
+++ data/mnist.py
@@ -6,29 +6,46 @@ import torch
 class Mnist4x4grid(Dataset):
 
     def __init__(self, opt):
-        self.dataset = datasets.MNIST(opt.dataroot,
+
+        self.datasets = []
+
+        self.datasets.append(datasets.MNIST(opt.dataroot,
                                       transform = transforms.Compose([
                                            transforms.ToTensor()
                                        ]),
                                       train = opt.is_train,
-                                      download = opt.download)
+                                      download = opt.download))
+
+        self.datasets.append(datasets.KMNIST(opt.dataroot,
+                                      transform = transforms.Compose([
+                                           transforms.ToTensor()
+                                       ]),
+                                      train = opt.is_train,
+                                      download = opt.download))
+
+        self.num_images = self.N
 
     def __len__(self):
-        return self.dataset.__len__()
+        return min([self.datasets[i].__len__() for i in range(self.num_images)])
 
     def __getitem__(self, idx):
 
-        im1, target1 = self.dataset.__getitem__(idx)
+        im = torch.zeros((1, 64,64), dtype=torch.float)
+        target = ''
+        for i in range(self.num_images):
+            im1, target1 = self.datasets[i].__getitem__(idx)
 
-        c, w,h = im1.shape
-        im = torch.zeros((c, 64,64), dtype=torch.float)
+            c, w,h = im1.shape
 
-        x = np.random.randint(0,64-w)
-        y = np.random.randint(0,64-h)
+            x = np.random.randint(0,64-w)
+            y = np.random.randint(0,64-h)
 
-        im[:,x:x+w, y:y+h] = im1.type(torch.float)
+            im[:,x:x+w, y:y+h] = im1.type(torch.float)
+            target += str(target1)
 
         transform = transforms.Compose([transforms.Normalize((0.1307,), (0.3081,))])
         im = transform(im)
 
-        return im, target1
+        target = sorted(target)
+
+        return im, target
