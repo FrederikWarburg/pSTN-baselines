e076dbf322e2cbd16d86a7a8cdab79b85fd30ed9
Frederik Warburg <frederikwarburg@Frederiks-MacBook-Pro-10.local>
2019-12-20 16:37:26 +0100

    test mnist experiment on cluster

--- models/inceptionlocalizer.py
+++ models/inceptionlocalizer.py
@@ -43,7 +43,7 @@ class InceptionLocalizer(nn.Module):
         if self.num_param == 2:
             #self.fc2.bias.data.normal_(0, 1).clamp_(min=-0.5,max=0.5)
             #self.fc2.bias.data.copy_(torch.tensor([0, 0], dtype=torch.float).repeat(self.N))
-            bias = torch.tensor([[-1,-1],[-1,1],[1,-1],[1,1]], dtype=torch.float)*0.5#*opt.crop_size)
+            bias = torch.tensor([[-1,-1],[-1,1],[1,-1],[1,1]], dtype=torch.float)*0.5
             self.fc2.bias.data.copy_(bias[:self.N].view(-1))
 
         elif self.num_param == 4:
--- models/simplelocalizer.py
+++ models/simplelocalizer.py
@@ -33,7 +33,12 @@ class SimpleLocalizer(nn.Module):
         # Initialize the weights/bias with identity transformation
         self.fc_loc[2].weight.data.zero_()
         if self.num_param == 2:
-            self.fc_loc[2].bias.data.copy_(torch.zeros(self.num_param*self.N, dtype=torch.float))
+            # Center initialization
+            #self.fc_loc[2].bias.data.copy_(torch.zeros(self.num_param*self.N, dtype=torch.float))
+
+            # Tiling
+            bias = torch.tensor([[-1,-1],[1,1],[1,-1],[-1,1]], dtype=torch.float)*0.5
+            self.fc2.bias.data.copy_(bias[:self.N].view(-1))
         if self.num_param == 6:
             self.fc_loc[2].bias.data.copy_(torch.tensor([1,0,0,
                                                          0,1,0]*self.N, dtype=torch.float))
