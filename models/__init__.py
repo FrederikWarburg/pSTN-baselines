7649dae23776f902a5de44ce2f86a7f645b72d28
Frederik Warburg <frederikwarburg@usb-nbcoko.imm.dtu.dk>
2020-01-17 10:06:19 +0100

    where do nan for stn come from

--- models/__init__.py
+++ models/__init__.py
@@ -121,11 +121,11 @@ class CoolSystem(pl.LightningModule):
         if self.opt.model.lower() in ['stn', 'pstn']:
             self.logger.experiment.add_image('grid_out', outputs[0]['grid_out'], self.global_step)
 
-
+            """
             if outputs[0]['bbox_viz'] is not None:
                 self.logger.experiment.add_image('bbox', outputs[0]['bbox_viz'], self.global_step)
 
-            """
+            
             if self.opt.model.lower() == 'stn':
                 mu_mean = torch.stack([x['theta'] for x in outputs]).mean(dim=0).mean(dim=0)
                 mu_std = torch.stack([x['theta'] for x in outputs]).std(dim=0).std(dim=0)
