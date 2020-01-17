9d459c02ffc36fd59699109c2bed0a9e35479520
Frederik Warburg <frederikwarburg@usb-nbcoko.imm.dtu.dk>
2020-01-17 09:35:46 +0100

    where do nan for stn come from

--- train2.py
+++ train2.py
@@ -53,10 +53,12 @@ if __name__ == '__main__':
 ########
 
 # simple classifier
-# python train2.py --dataroot ../data/ --model cnn --basenet simple --dataset mnist --digits 2 --N 1 --train_samples 1 --test_samples 1 --batch_size 256 --num_classes 100
+# python train2.py --dataroot ../data/ --model cnn --basenet simple --dataset mnist --digits 2 --N 1 --train_samples 1 --test_samples 1 --batch_size 256 --num_classes 100 --epochs 50
 
 # simple stn
 # python train2.py --dataroot /scratch/s153847/ --model stn --basenet simple --dataset mnist --digits 2 --N 2 --train_samples 1 --test_samples 1 --batch_size 256 --num_classes 100 --step_size 3 --smallest_size 64 --crop_size 64 --lr_loc 1e-02 --seed 42 --lr 0.1
 
 # simple pstn
-# python train2.py --dataroot /scratch/s153847/ --model pstn --basenet simple --dataset mnist --digits 2 --N 2 --train_samples 2 --test_samples 10 --batch_size 256 --num_classes 100 --step_size 1000 --smallest_size 64 --crop_size 64 --lr_loc 1e-02 --seed 42 --lr 0.1 --criterion elbo
+# python train2.py --dataroot /scratch/s153847/ --model pstn --basenet simple --dataset mnist --digits 2 --N 2 --train_samples 2 --test_samples 10 --batch_size 256 --num_classes 100 --step_size 3 --smallest_size 64 --crop_size 64 --lr_loc 1e-02 --seed 42 --lr 0.1 --criterion elbo
+
+# Note that we received significantly better results samples 2 times during training. I conducted several experiments with only one sample during training, but haven't been able to achieve better results.
