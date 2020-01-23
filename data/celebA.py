9878ca7210aa2aad131905015123b1f81bfe9a58
Frederik Warburg <frederikwarburg@usb-nbcoko.imm.dtu.dk>
2020-01-23 15:41:34 +0100

    celeba

--- data/celebA.py
+++ data/celebA.py
@@ -6,12 +6,18 @@ import torch
 from functools import partial
 import torch
 import os
-import PIL
-import pandas
-from torchvision.datasets.vision import VisionDataset
+from PIL import Image
+import pandas as pd
 
-class MyCelebA(Dataset):
+attribute_map = ['5_o_Clock_Shadow','Arched_Eyebrows','Attractive','Bags_Under_Eyes','Bald','Bangs','Big_Lips',
+                 'Big_Nose','Black_Hair','Blond_Hair','Blurry','Brown_Hair','Bushy_Eyebrows','Chubby',
+                 'Double_Chin','Eyeglasses','Goatee','Gray_Hair','Heavy_Makeup','High_Cheekbones','Male',
+                 'Mouth_Slightly_Open','Mustache','Narrow_Eyes','No_Beard','Oval_Face','Pale_Skin',
+                 'Pointy_Nose','Receding_Hairline','Rosy_Cheeks','Sideburns','Smiling','Straight_Hair',
+                 'Wavy_Hair','Wearing_Earrings','Wearing_Hat','Wearing_Lipstick','Wearing_Necklace',
+                 'Wearing_Necktie','Young']
 
+class celebA(torch.utils.data.Dataset):
     def __init__(self, opt, data_div):
 
         if data_div == 0:
@@ -22,77 +28,24 @@ class MyCelebA(Dataset):
             split = 'test'
 
         if data_div == 0:
-            transform = transforms.Compose([transforms.Resize((64,73)), transforms.RandomCrop((64,64)), transforms.ToTensor()])
+            self.transform = transforms.Compose([transforms.Resize((64,73)), transforms.RandomCrop((64,64)), transforms.ToTensor()])
         else:
-            transform = transforms.Compose([transforms.Resize((64,73)), transforms.CenterCrop((64,64)), transforms.ToTensor()])
+            self.transform = transforms.Compose([transforms.Resize((64,73)), transforms.CenterCrop((64,64)), transforms.ToTensor()])
 
-        self.datasets = CelebA(opt.dataroot,
-                              transform = transform,
-                              split = split,
-                              download = opt.download)
-
-    def __len__(self):
-        return self.datasets.__len__()
-
-    def __getitem__(self, idx):
-
-        return self.datasets.__getitem__(idx)
+        self.root = opt.dataroot
+        self.base_folder = 'celeba'
 
 
-class CelebA(VisionDataset):
-    """`Large-scale CelebFaces Attributes (CelebA) Dataset <http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html>`_ Dataset.
-    Args:
-        root (string): Root directory where images are downloaded to.
-        split (string): One of {'train', 'valid', 'test', 'all'}.
-            Accordingly dataset is selected.
-        target_type (string or list, optional): Type of target to use, ``attr``, ``identity``, ``bbox``,
-            or ``landmarks``. Can also be a list to output a tuple with all specified target types.
-            The targets represent:
-                ``attr`` (np.array shape=(40,) dtype=int): binary (0, 1) labels for attributes
-                ``identity`` (int): label for each person (data points with the same identity are the same person)
-                ``bbox`` (np.array shape=(4,) dtype=int): bounding box (x, y, width, height)
-                ``landmarks`` (np.array shape=(10,) dtype=int): landmark points (lefteye_x, lefteye_y, righteye_x,
-                    righteye_y, nose_x, nose_y, leftmouth_x, leftmouth_y, rightmouth_x, rightmouth_y)
-            Defaults to ``attr``. If empty, ``None`` will be returned as target.
-        transform (callable, optional): A function/transform that  takes in an PIL image
-            and returns a transformed version. E.g, ``transforms.ToTensor``
-        target_transform (callable, optional): A function/transform that takes in the
-            target and transforms it.
-        download (bool, optional): If true, downloads the dataset from the internet and
-            puts it in root directory. If dataset is already downloaded, it is not
-            downloaded again.
-    """
+        self.fn = partial(os.path.join, self.root, self.base_folder)
+        csv_file = pd.read_csv(self.fn("list_attr_celeba.txt"))
+        splits = pd.read_csv(self.fn("list_eval_partition.txt"))
 
-    base_folder = "celeba"
-    # There currently does not appear to be a easy way to extract 7z in python (without introducing additional
-    # dependencies). The "in-the-wild" (not aligned+cropped) images are only in 7z, so they are not available
-    # right now.
-    file_list = [
-        # File ID                         MD5 Hash                            Filename
-        ("0B7EVK8r0v71pZjFTYXZWM3FlRnM", "00d2c5bc6d35e252742224ab0c1e8fcb", "img_align_celeba.zip"),
-        # ("0B7EVK8r0v71pbWNEUjJKdDQ3dGc", "b6cd7e93bc7a96c2dc33f819aa3ac651", "img_align_celeba_png.7z"),
-        # ("0B7EVK8r0v71peklHb0pGdDl6R28", "b6cd7e93bc7a96c2dc33f819aa3ac651", "img_celeba.7z"),
-        ("0B7EVK8r0v71pblRyaVFSWGxPY0U", "75e246fa4810816ffd6ee81facbd244c", "list_attr_celeba.txt"),
-        ("1_ee_0u7vcNLOfNLegJRHmolfH5ICW-XS", "32bd1bd63d3c78cd57e08160ec5ed1e2", "identity_CelebA.txt"),
-        ("0B7EVK8r0v71pbThiMVRxWXZ4dU0", "00566efa6fedff7a56946cd1c10f1c16", "list_bbox_celeba.txt"),
-        ("0B7EVK8r0v71pd0FJY3Blby1HUTQ", "cc24ecafdb5b50baae59b03474781f8c", "list_landmarks_align_celeba.txt"),
-        # ("0B7EVK8r0v71pTzJIdlJWdHczRlU", "063ee6ddb681f96bc9ca28c6febb9d1a", "list_landmarks_celeba.txt"),
-        ("0B7EVK8r0v71pY0NSMzRuSXJEVkk", "d32c9cbf5e040fd4025c592c306e6668", "list_eval_partition.txt"),
-    ]
+        target = attribute_map[opt.target_attr]
+        print("target attribute ==> ", target)
+        target = csv_file[target].values
+        target[target == -1] = 0
 
-    def __init__(self, root, split="train", target_type="attr", transform=None,
-                 target_transform=None, download=False):
-
-        super(CelebA, self).__init__(root, transform=transform,
-                                     target_transform=target_transform)
-        self.split = split
-        if isinstance(target_type, list):
-            self.target_type = target_type
-        else:
-            self.target_type = [target_type]
-
-        if not self.target_type and self.target_transform is not None:
-            raise RuntimeError('target_transform is specified but target_type is empty')
+        filename = csv_file['image_id'].values
 
         split_map = {
             "train": 0,
@@ -100,62 +53,23 @@ class CelebA(VisionDataset):
             "test": 2,
             "all": None,
         }
-
-        split = split_map[self.split.lower()]
-
-        fn = partial(os.path.join, self.root, self.base_folder)
-        splits = pandas.read_csv(fn("list_eval_partition.txt"), delim_whitespace=False, header=0)
-        #identity = pandas.read_csv(fn("identity_CelebA.txt"), delim_whitespace=True, header=None, index_col=0)
-        bbox = pandas.read_csv(fn("list_bbox_celeba.txt"), delim_whitespace=False, header=0)
-        landmarks_align = pandas.read_csv(fn("list_landmarks_align_celeba.txt"), delim_whitespace=False, header=0)
-        attr = pandas.read_csv(fn("list_attr_celeba.txt"), delim_whitespace=False, header=0)
+        split = split_map[split.lower()]
 
         mask = slice(None) if split is None else (splits['partition'] == split)
 
-        self.filename = splits[mask]['image_id'].values
+        self.filename = filename[mask]
+        self.target = target[mask]
 
-        #self.identity = torch.as_tensor(identity[mask].values)
-        self.bbox = torch.as_tensor(bbox[mask].values[:,1:].astype(np.uint8))
-        self.landmarks_align = torch.as_tensor(landmarks_align[mask].values[:,1:].astype(np.uint8))
-        self.attr = torch.as_tensor(attr[mask].values[:,1:].astype(np.uint8))
-        self.attr = (self.attr + 1) // 2  # map from {-1, 1} to {0, 1}
-        self.attr_names = list(attr.columns)
-
-
-    def __getitem__(self, index):
-        X = PIL.Image.open(os.path.join(self.root, self.base_folder, "img_align_celeba", self.filename[index]))
-
-        target = []
-        for t in self.target_type:
-            if t == "attr":
-                target.append(self.attr[index, :])
-            #elif t == "identity":
-            #    target.append(self.identity[index, 0])
-            elif t == "bbox":
-                target.append(self.bbox[index, :])
-            elif t == "landmarks":
-                target.append(self.landmarks_align[index, :])
-            else:
-                # TODO: refactor with utils.verify_str_arg
-                raise ValueError("Target type \"{}\" is not recognized.".format(t))
-
-        if self.transform is not None:
-            X = self.transform(X)
-
-        if target:
-            target = tuple(target) if len(target) > 1 else target[0]
-
-            if self.target_transform is not None:
-                target = self.target_transform(target)
-        else:
-            target = None
-
-        return X, target
+        self.transform = transforms.Compose([transforms.Resize((64,73)), transforms.RandomCrop((64,64)), transforms.ToTensor()])
 
     def __len__(self):
-        return len(self.attr)
+        return len(self.target)
 
-    def extra_repr(self):
-        lines = ["Target type: {target_type}", "Split: {split}"]
-        return '\n'.join(lines).format(**self.__dict__)
+    def __getitem__(self, idx):
 
+        image = Image.open(self.fn("img_align_celeba",  self.filename[idx]))
+        target = self.target[idx]
+
+        image = self.transform(image)
+
+        return image, target.astype('long')
