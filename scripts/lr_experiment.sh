2c0af695d7183ad3cd72e7f12ca5cabc8a172e21
Frederik Warburg <frederikwarburg@Frederiks-MacBook-Pro-10.local>
2020-02-15 17:16:53 +0100

    moved shell scripts to folder

---
+++ scripts/lr_experiment.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+DATAPATH="/scratch/s153847/"
+MODELS=("stn" "pstn")
+TEST_SAMPELS=(1 10)
+TRAIN_SAMPELS=(1 1)
+CRITERION=("nll" "elbo")
+LEARNING_RATES=(1e-1 1e-2 1e-3 1e-4 1e-5)
+
+for LR in {0..4}
+do
+    for MODEL in {0..1}
+    do
+        echo ${MODELS[$MODEL]}
+        echo $ATTR
+        echo ${TEST_SAMPELS[$MODEL]}
+        echo ${TRAIN_SAMPELS[$MODEL]}
+        echo ${CRITERION[$MODEL]}
+        CUDA_VISIBLE_DEVICES=2 python train2.py --dataroot $DATAPATH \
+                        --dataset "celeba" \
+                        --attr 15 \
+                        --batch_size 256 \
+                        --num_classes 2 \
+                        --num_threads 1 \
+                        --epochs 10 \
+                        --step_size 9999 \
+                        --seed 42 \
+                        --model ${MODELS[$MODEL]} \
+                        --num_param 6 \
+                        --N 1 \
+                        --test_samples ${TEST_SAMPELS[$MODEL]} \
+                        --train_samples ${TRAIN_SAMPELS[$MODEL]} \
+                        --criterion ${CRITERION[$MODEL]} \
+                        --lr ${LEARNING_RATES[$LR]} \
+                        --lr_loc 0.1 \
+                        --sigma 0.1 \
+                        --smallest_size 64 \
+                        --crop_size 64 \
+                        --run_test_freq 1 \
+                        --basenet "simple" \
+                        --digits 1 \
+                        --trainval_split True \
+                        --save_results True \
+                        --savepath 'lr_experiment'
+    done
+done
+
---
+++ scripts/mnist_experiment.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+DATAPATH="/scratch/s153847/data/"
+MODELS=("cnn" "stn" "pstn")
+BRANCHES=(1 2 2)
+PARAMS=(1 2 2)
+TEST_SAMPELS=(0 0 10)
+TRAIN_SAMPELS=(1 1 2)
+CRITERION=("nll" "nll" "elbo")
+
+for MODEL in 0 1 2
+do
+    echo ${MODELS[$MODEL]}
+    echo ${TRAIN_SAMPELS[$MODEL]}
+    CUDA_VISIBLE_DEVICES=4 python train.py --dataroot $DATAPATH \
+                    --dataset "mnist_easy" \
+                    --batch_size 256 \
+                    --num_classes 100 \
+                    --num_threads 8 \
+                    --epochs 10 \
+                    --step_size 3 \
+                    --seed 42 \
+                    --model ${MODELS[$MODEL]} \
+                    --num_param ${PARAMS[$MODEL]} \
+                    --N ${BRANCHES[$MODEL]} \
+                    --test_samples ${TEST_SAMPELS[$MODEL]} \
+                    --train_samples ${TRAIN_SAMPELS[$MODEL]} \
+                    --criterion ${CRITERION[$MODEL]} \
+                    --visualize True \
+                    --lr 0.1 \
+                    --sigma 0.1 \
+                    --smallest_size 64 \
+                    --crop_size 64 \
+                    --run_test_freq 1 \
+                    --num_param 2 \
+                    --lr_loc 0.01 \
+                    --basenet "simple" \
+                    --alpha 0.1 \
+		            --digits 2
+done
