bc06f686106bdbd9e37fc89abe36ed68b31ca79f
Frederik Warburg <frederikwarburg@Frederiks-MacBook-Pro-10.local>
2020-05-11 16:46:27 +0200

    remove dropout from localizer

--- models/celebaclassifier.py
+++ models/celebaclassifier.py
@@ -12,7 +12,7 @@ class CelebaClassifier(nn.Module):
         # hyper parameters
         self.N = opt.N
         self.S = opt.test_samples
-        self.dropout_rate = opt.dropout_rate
+        #self.dropout_rate = opt.dropout_rate
         self.train_samples = opt.train_samples
         self.test_samples = opt.test_samples
         self.feature_size = 640
