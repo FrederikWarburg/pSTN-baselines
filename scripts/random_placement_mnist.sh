0d246fd27d6b2d7664c43dac4f732b6272dc5f5f
Frederik Warburg <frwa@comp-pcfrwa.compute.dtu.dk>
2020-05-25 20:11:23 +0200

    should work

--- scripts/random_placement_mnist.sh
+++ scripts/random_placement_mnist.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
-DATAPATH="../data/"
+DATAPATH="../../data/"
 MODELS=("cnn" "cnn" "stn" "stn" "pstn")
 BRANCHES=(1 1 1 1 1)
 PARAMS=(1 1 2 2 2)
 TEST_SAMPELS=(1 1 1 1 10)
 TRAIN_SAMPELS=(1 1 1 1 2)
 CRITERION=("nll" "nll" "nll" "nll" "elbo")
-GPUS=(1 2 3 4 1 2 3 4)
+GPUS=(0 0 0 0 0 0 0 1 2 3 4 1 2 3 4)
 DATAAUGMENTATION=("None" "standard" "None" "standard" "None")
 
-for MODEL in 4
+for MODEL in 2
 do
     echo $MODEL
     echo ${MODELS[$MODEL]}
     echo ${TRAIN_SAMPELS[$MODEL]}
-    # OMP_NUM_THREADS=2 CUDA_VISIBLE_DEVICES=${GPUS[$MODEL]}
-    CUDA_VISIBLE_DEVICES=5 python train.py --dataroot $DATAPATH \
+    OMP_NUM_THREADS=2 CUDA_VISIBLE_DEVICES=${GPUS[$MODEL]} python train.py --dataroot $DATAPATH \
                     --dataset "mnistxkmnist" \
                     --batch_size 256 \
                     --num_classes 10 \
@@ -38,8 +37,7 @@ do
         		    --trainval_split True \
                     --moving_mean True \
                     --save_results True \
-                    --save_training_theta True \
                     --theta_path 'random_placement_mnist' \
                     --download True \
-                    --lr_loc 1
+                    --lr_loc 0.01 &
 done
